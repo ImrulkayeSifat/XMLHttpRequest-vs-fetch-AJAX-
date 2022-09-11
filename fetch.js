8af83d64e0845c8d9b3abba62876179c2885e92f
ImrulkayeSifat <kayesimrul224@gmail.com>
2022-09-11 17:31:57 +0600

    fetch post request

--- fetch.js
+++ fetch.js
@@ -2,12 +2,25 @@ const url = 'https://jsonplaceholder.typicode.com/users'
 
 
 function myAxios(method,url,body=null){
-  return fetch(url).then((res)=>{
+  const headers = {
+    'Content-Type':'application/json'
+  }
+  return fetch(url,{
+    method:method,
+    headers:headers,
+    body:JSON.stringify(body)
+  }).then((res)=>{
     return res.json();
   })
 }
 
-myAxios('GET',url).then((res)=>{
+// myAxios('GET',url).then((res)=>{
+//   console.log(res);
+// }).catch(err=>{
+
+// })
+
+myAxios('POST',url,{name:'sifat',job:'software developer'}).then((res)=>{
   console.log(res);
 }).catch(err=>{
 
